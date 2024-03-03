c715b784a444edb86ecb12ea0edc879455cab2bf
Dmitriy <dima.kolmogorov.1993@gmail.com>
2024-03-03 17:25:03 +0300

    Создал программу, указанную в задании

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputArray = new string[] { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia","Denmark"};
+        string[] outputArray = new string[inputArray.Length];
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                outputArray[i] = inputArray[i];
+            }
+        }
+
+        for (int i = 0; i < outputArray.Length; i++)
+        {
+            Console.WriteLine(outputArray[i]);
+        }
+    }
+}
+
